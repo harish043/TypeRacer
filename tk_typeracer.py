832bc282ec71e6d75eb650a457d795acd6155929
Harish Balasubramanian <90543852+harish043@users.noreply.github.com>
2023-05-29 12:32:52 +0530

    Update tk_typeracer.py
    Simplified the code and made it more user friendly.

--- tk_typeracer.py
+++ tk_typeracer.py
@@ -1,57 +1,63 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 from timeit import default_timer as timer
 import random
 
-root = Tk()
-root.title("Typing Speed")
+class TypingSpeedApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Typing Speed")
 
-frame1 = Frame(root, bg="black")
-frame1.pack()
-begin = 0
-pangrams = ["The quick brown fox jumped over the lazy dog.", "Glib jocks quiz nymph to vex dwarf.",
-            "Sphinx of black quartz, judge my vow.", "How quickly daft jumping zebras vex!",
-            "The five boxing wizards jump quickly."]
-selecSentence = random.sample(pangrams, k=1)[0]
-no_chars = len(selecSentence)
+        self.frame1 = tk.Frame(self.root, bg="black")
+        self.frame1.pack()
+        self.start_time = 0
+        self.pangrams = [
+            "The quick brown fox jumped over the lazy dog.",
+            "Glib jocks quiz nymph to vex dwarf.",
+            "Sphinx of black quartz, judge my vow.",
+            "How quickly daft jumping zebras vex!",
+            "The five boxing wizards jump quickly."
+        ]
+        self.selected_sentence = random.choice(self.pangrams)
+        self.no_chars = len(self.selected_sentence)
 
+        self.dispLabel = tk.Label(self.frame1, text=self.selected_sentence, fg="white", bg="black")
+        self.dispLabel.grid(row=0, column=0, columnspan=4)
 
-def start():
-    if entry1.get() != "":
-        messagebox.askretrycancel("error", "You started the test after typing ")
-    else:
-        begin = timer()
+        self.entry1 = tk.Entry(self.frame1, width=50)
+        self.entry1.grid(row=1, column=0)
 
+        self.endButton = tk.Button(self.frame1, text="End", fg="white", bg="black", command=self.end)
+        self.endButton.grid(row=3, column=0, sticky="news")
 
-def end():
-    global begin, no_chars
+        self.startButton = tk.Button(self.frame1, text="Start", fg="white", bg="black", command=self.start)
+        self.startButton.grid(row=2, column=0, sticky="news")
 
-    final = entry1.get()
-    if final == "":
-        messagebox.showerror("Error", "The sentence has not been typed, please try again.")
-    else:
-        complete = timer()
-        elapsed = complete - begin
-        correct = 0
-        wrong = 0
-        for i in range(len(selecSentence)):
-            if final[i] == selecSentence[i]:
-                correct += 1
-            else:
-                wrong += 1
-        accuracy = (correct / no_chars) * 100
-        messagebox.showinfo("Success",
-                            "Your time was " + str(elapsed) + " seconds and your accuracy was " + str(accuracy) + "%")
-        root.quit()
+    def start(self):
+        if self.entry1.get() != "":
+            messagebox.askretrycancel("Error", "You started the test after typing")
+        else:
+            self.start_time = timer()
 
+    def end(self):
+        final = self.entry1.get()
+        if final == "":
+            messagebox.showerror("Error", "The sentence has not been typed, please try again.")
+        else:
+            complete = timer()
+            elapsed = complete - self.start_time
+            correct = sum(final[i] == self.selected_sentence[i] for i in range(len(self.selected_sentence)))
+            accuracy = (correct / self.no_chars) * 100
+            messagebox.showinfo(
+                "Success",
+                f"Your time was {elapsed:.2f} seconds and your accuracy was {accuracy:.2f}%"
+            )
+            self.root.quit()
 
-dispLabel = Label(frame1, text=selecSentence, fg="white", bg="black")
-dispLabel.grid(row=0, column=0, columnspan=4)
-entry1 = Entry(frame1, width=50)
-entry1.grid(row=1, column=0)
-endButton = Button(frame1, text="End", fg="white", bg="black", command=end)
-endButton.grid(row=3, column=0, sticky="news")
-startButton = Button(frame1, text="Start", fg="white", bg="black", command=start)
-startButton.grid(row=2, column=0, sticky="news")
+    def run(self):
+        self.root.mainloop()
 
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TypingSpeedApp(root)
+    app.run()
